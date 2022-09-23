8bec957d76f1f2791c7ab5db83623df00a9e5213
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2022-09-23 16:10:41 +0530

    contentful working

--- pages/articles.js
+++ pages/articles.js
@@ -1,61 +1,29 @@
 /** @format */
 
 import { createClient } from "contentful";
-import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
-import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
+
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Articles({ articles }) {
-  console.log(articles);
-
-  const options = {
-    renderNode: {
-      [BLOCKS.HEADING_1]: (node, next) => `<h1>${next(node.content)}</h1>`,
-      [BLOCKS.HEADING_2]: (node, next) => `<h2>${next(node.content)}</h2>`,
-      [BLOCKS.HEADING_3]: (node, next) => `<h3>${next(node.content)}</h3>`,
-      [BLOCKS.HEADING_4]: (node, next) => `<h4>${next(node.content)}</h4>`,
-      [BLOCKS.HEADING_5]: (node, next) => `<h5>${next(node.content)}</h5>`,
-      [BLOCKS.HEADING_6]: (node, next) => `<h6>${next(node.content)}</h6>`,
-      [BLOCKS.PARAGRAPH]: (node, next) => `<p>${next(node.content)}</p>`,
-      [BLOCKS.EMBEDDED_ASSET]: (node) => {
-        return `<Image src="${node.data.target.fields.file.url}" width = {375} height= {150} alt="Image"/>`;
-      },
-      [BLOCKS.UL_LIST]: (node, next) => `<ul>${next(node.content)}</ul>`,
-      [BLOCKS.OL_LIST]: (node, next) => `<ol>${next(node.content)}</ol>`,
-      [BLOCKS.LIST_ITEM]: (node, next) => `<li>${next(node.content)}</li>`,
-      [BLOCKS.QUOTE]: (node, next) =>
-        `<blockquote>${next(node.content)}</blockquote>`,
-      [BLOCKS.EMBEDDED_ENTRY]: (node) => {},
-      [BLOCKS.HR]: () => `<hr />`,
-      [INLINES.HYPERLINK]: (node) => {
-        return `<a href="${node.data.uri}">${node.content[0].value}</a>`;
-      },
-      [INLINES.ENTRY_HYPERLINK]: (node) => {
-        return `<a href="${node.data.uri}">${node.content[0].value}</a>`;
-      },
-    },
-  };
   return (
     <main className={styles.main}>
       <h1>Summaries</h1>
       {articles.map((article) => (
-        <div key={article.sys.id} className={styles.article}>
-          <Image
-            src={`https:${article.fields.image.fields.file.url}`}
-            width={article.fields.image.fields.file.details.image.width}
-            height={article.fields.image.fields.file.details.image.height}
-            alt="Banner"
-            className={styles.image}
-          />
-          <h1>{article.fields.title}</h1>
-          <p>{article.fields.brief}</p>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: documentToHtmlString(article.fields.summary, options),
-            }}
-          />
-        </div>
+        <Link href={`/blog/${article.fields.slug}`} key={article.fields.id}>
+          <div className={styles.article}>
+            <Image
+              src={`https:${article.fields.image.fields.file.url}`}
+              width={article.fields.image.fields.file.details.image.width}
+              height={article.fields.image.fields.file.details.image.height}
+              alt="Banner"
+              className={styles.image}
+            />
+            <h1>{article.fields.title}</h1>
+            <p>{article.fields.brief}</p>
+          </div>
+        </Link>
       ))}
     </main>
   );
@@ -75,5 +43,6 @@ export async function getStaticProps() {
     props: {
       articles: res.items,
     },
+    revalidate: 24 * 60 * 60,
   };
 }
---
+++ pages/blog/[slug].jsx
@@ -0,0 +1,103 @@
+import { createClient } from "contentful";
+import styles from "../../styles/Home.module.css";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+import Image from "next/image";
+
+const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID??'',
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN??'',
+  });
+
+export const getStaticPaths = async () => {
+    const res = await client.getEntries({
+        content_type: "booksInShort",
+      });
+    const paths = res.items.map((item) => {
+        return{
+            params: {slug: item.fields.slug}
+        }
+    });
+    return {
+        paths,
+        fallback: false,
+        revalidate: 24 * 60 * 60,
+    }
+}
+
+
+export const getStaticProps = async (context) => {
+    
+    const {params} = context;
+    const {items} = await client.getEntries({
+        content_type: "booksInShort",
+        'fields.slug': params.slug
+    });
+    
+    return {
+        props: {
+            blog: items[0]
+        },
+        revalidate: 24 * 60 * 60,
+    }
+}
+
+
+const Blog = ({blog}) => {
+    const {title, summary, image} = blog.fields;
+    const options = {
+        renderNode: {
+          [BLOCKS.HEADING_1]: (node, next) => `<h1>${next(node.content)}</h1>`,
+          [BLOCKS.HEADING_2]: (node, next) => `<h2>${next(node.content)}</h2>`,
+          [BLOCKS.HEADING_3]: (node, next) => `<h3>${next(node.content)}</h3>`,
+          [BLOCKS.HEADING_4]: (node, next) => `<h4>${next(node.content)}</h4>`,
+          [BLOCKS.HEADING_5]: (node, next) => `<h5>${next(node.content)}</h5>`,
+          [BLOCKS.HEADING_6]: (node, next) => `<h6>${next(node.content)}</h6>`,
+          [BLOCKS.PARAGRAPH]: (node, next) => `<p>${next(node.content)}</p>`,
+          [BLOCKS.EMBEDDED_ASSET]: (node) => {
+            return `<Image src="${node.data.target.fields.file.url}" width = {375} height= {150} alt="Image"/>`;
+          },
+          [BLOCKS.UL_LIST]: (node, next) => `<ul>${next(node.content)}</ul>`,
+          [BLOCKS.OL_LIST]: (node, next) => `<ol>${next(node.content)}</ol>`,
+          [BLOCKS.LIST_ITEM]: (node, next) => `<li>${next(node.content)}</li>`,
+          [BLOCKS.QUOTE]: (node, next) =>
+            `<blockquote>${next(node.content)}</blockquote>`,
+          [BLOCKS.EMBEDDED_ENTRY]: (node) => {},
+          [BLOCKS.HR]: () => `<hr />`,
+          [INLINES.HYPERLINK]: (node) => {
+            return `<a href="${node.data.uri}">${node.content[0].value}</a>`;
+          },
+          [INLINES.ENTRY_HYPERLINK]: (node) => {
+            return `<a href="${node.data.uri}">${node.content[0].value}</a>`;
+          },
+        },
+      };
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1 style={{
+                    textAlign: "center",
+                    margin: "4rem 1rem 2rem 1rem",
+                }}>
+                    {title}
+                </h1>
+                <article className={styles.article}>
+                    <div className={styles.image}>
+                        <Image src={"https:" + image.fields.file.url} width = {375} height= {150} alt="Image"/>
+                    </div>
+                <div
+                    dangerouslySetInnerHTML={
+                        {__html: documentToHtmlString(summary, options)}
+                    }
+                />
+                </article>
+
+            </main>
+        </div>
+    );
+}
+
+
+
+
+export default Blog;
