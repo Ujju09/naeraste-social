3c8a041e6ca787e9850534eb73dca926e560c0c1
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2022-12-13 12:45:22 +0530

    experimental set off

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ const nextConfig = {
   images: {
     domains: ["images-na.ssl-images-amazon.com", "images.ctfassets.net"],
   },
-  experimental: {
-    appDir: true,
-
-  },
 };
 
 module.exports = nextConfig;
